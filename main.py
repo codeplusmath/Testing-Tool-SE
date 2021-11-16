035cd42566fe8a76da4055723329f88ea9e2e065
codeplusmath <sawantrv19.comp@coep.ac.in>
2021-11-16 10:48:55 +0530

    Some Changes

--- main.py
+++ main.py
@@ -83,13 +83,28 @@ class ScholarshipForm():
 
 
     def viewdata(self):
-        path = f'./db/{self.name}.dat'
-        if os.path.exits(path):
+        name = input("Enter name: ")
+        path = f'./db/{name}.dat'
+        if os.path.exists(path):
             f = open(path, 'r')
             print(f)
             print('\n')
         else:
             print('No application found')
+            
+    def removedata(self):
+        name = input("Enter name: ")
+        path = f'./db/{name}.dat'
+        if os.path.exists(path):
+            os.remove(path)
+            print('Application removed successfully...\n')
+        else:
+            print('No application found')
+    
+    def alldata(self):
+        path = f'./db/'
+        for f in os.listdir(path):
+            print(f)
 
 
     def updatedata(self):
@@ -114,3 +129,59 @@ class ScholarshipForm():
         except:
             print('cannot update')
 
+    def menu(self):
+        print("\n\t0.Menu \n\t1.Add Form \n\t2.Show Form Data \n\t3.Show All Forms \n\t4.Remove Data \n\t5.Exit")
+        while True:    
+            choice = int(input("\nEnter Choice: "))
+            if (choice == 0):
+                self.menu()
+                
+            elif (choice == 1):
+                print('-------Academic Details-------')
+                name = input('Enter Name: ')
+                email = input('Enter EmailID: ')
+                phoneno = int(input('Enter Phone NO: '))
+                address = input('Enter Address: ') 
+                DOB = str(input('Birth Date (dd-mm-yyyy)'))
+                self.insert_personaldetails(name, email, phoneno, address, DOB)
+                
+                print('-------Academic Details-------')
+                collegename = input('Enter College Name: ')
+                branch = input('Enter Branch: ')
+                currentyear = int(input('Enter Current Year: '))
+                cgpa = float(input('Enter Cgpa: '))
+                self.insert_academicdetails(collegename, branch, currentyear, cgpa)
+                
+                print('-------Family Details-------')
+                noofmembers = int(input('Enter No of family members: '))
+                fathername = input('Enter Father Name')
+                fatheroccupation = input('Enter Father Occupation: ')
+                fathereducation = input('Enter Father Education')
+                mothername = input('Enter Mother Name')
+                motheroccupation = input('Enter Mother Occupation: ')
+                mothereducation = input('Enter Mother Education')
+                annualincome = int(input('Enter Annual Income: '))
+                self.insert_familydetails(noofmembers, fathername, fatheroccupation, fathereducation, mothername, motheroccupation, mothereducation, annualincome)
+                self.addingDataToFile()
+            
+            elif (choice == 2):
+                self.viewdata()
+            
+            elif (choice == 3):
+                self.alldata()
+            
+            elif (choice == 4):
+                self.removedata()
+            
+            else:
+                print("\nThank You...")
+                exit(0)
+        return 0
+        
+def main():
+    form = ScholarshipForm()
+    form.menu()
+    return 0
+ 
+if __name__ == '__main__':
+    main()
